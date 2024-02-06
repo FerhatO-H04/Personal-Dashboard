2ca82d82234eb4920f0013eff1ace3f7d62f8506
FerhatO-H04 <131246514+FerhatO-H04@users.noreply.github.com>
2024-02-06 23:55:00 +0100

    Created Personal Dashboard (Coins)

---
+++ index.js
@@ -0,0 +1,115 @@
+// Unsplas API
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(res => res.json())
+    .then(data => {
+        // Set the background image of the body
+        document.body.style.backgroundImage = `url(${data.urls.regular})`;
+        // Set the author information
+        document.getElementById("author").textContent = `By: ${data.user.name}`;
+    })
+    .catch(err => {
+        // Use a default background image/author in case of an error
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`;
+        document.getElementById("author").textContent = `By: Dodi Achmad`;
+    });
+
+// CoinGecko API Dogecoin
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong");
+        }
+        return res.json();
+    })
+    .then(data => {
+        // Display the cryptocurrency information
+        document.getElementById("crypto-top-dogecoin").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `;
+        document.getElementById("crypto-dogecoin").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
+        `;
+    })
+    .catch(err => console.error(err));
+
+// CoinGecko API Bitcoin
+fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong");
+        }
+        return res.json();
+    })
+    .then(data => {
+        // Display the cryptocurrency information
+        document.getElementById("crypto-top-bitcoin").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `;
+        document.getElementById("crypto-bitcoin").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
+        `;
+    })
+    .catch(err => console.error(err));
+
+// CoinGecko API Ethereum
+fetch("https://api.coingecko.com/api/v3/coins/ethereum")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong");
+        }
+        return res.json();
+    })
+    .then(data => {
+        // Display the cryptocurrency information
+        document.getElementById("crypto-top-ethereum").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `;
+        document.getElementById("crypto-ethereum").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
+        `;
+    })
+    .catch(err => console.error(err));
+
+// Weather API
+function getCurrentTime() {
+    const date = new Date();
+    // Display the current time
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", { timeStyle: "short" });
+}
+
+setInterval(getCurrentTime, 1000);
+
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Weather data not available");
+            }
+            return res.json();
+        })
+        .then(data => {
+            // Get the Fahrenheit temperature
+            const fahrenheitTemperature = data.main.temp;
+
+            // Convert Fahrenheit to Celsius and round it
+            const celsiusTemperature = Math.round((fahrenheitTemperature - 32) * (5 / 9));
+
+            // Display the weather information
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+            document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+                <p class="weather-temp">${celsiusTemperature}ºC</p>
+                <p class="weather-city">${data.name}</p>
+            `;
+        })
+        .catch(err => console.error(err));
+});
